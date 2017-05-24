61b5deb5c45343402303afe2d4107651c583c752
wgwz <klawlor419@gmail.com>
2017-05-24 19:37:06 -0400

    current version of the data model
    - includes a `build_model` function which will populate a graph with
      some example data
    - also adds in an endpoint that will run the `build_model` function
    - just send a GET request to http://<host>:5000/build (can be done
      from a browser)
    
    notes to self:
      - don't forget some of the naming that you want to change
      - one of the next steps should be getting tests set up
      - another thing to keep in mind is that the structures continue
        to be built correctly as you add in more data

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_py2neo import Py2Neo
 
 app = Flask(__name__)
 app.config.update({
   'PY2NEO_HOST': 'neo4j'    
 })
-
 db = Py2Neo(app)
 
 
 class Client(db.Model):
 
-    __primarykey__ = 'id'
+    __primarykey__ = 'company_id'
 
     person = db.Label()
-    id = db.Property()
-    email = db.Property()
+    
+    company_id = db.Property()
+    company_name = db.Property()
+
     has_onboard = db.RelatedTo('Onboard')
 
     @staticmethod
-    def create(client_id, client_email):
+    def create(company_id, company_name):
         client = Client()
         client.person = True
-        client.email = client_email
-        client.id = client_id
+        client.company_id = company_id
+        client.company_name = company_name
         db.graph.create(client)
         return client
 
@@ -45,13 +46,15 @@ class Step(db.Model):
 
     task_name = db.Property()
     step_number = db.Property()
+    duration = db.Property()
     completed = db.Property()
 
     @staticmethod
-    def create(task_name, step_number):
+    def create(task_name, step_number, step_duration):
         step = Step()
         step.task_name = task_name
         step.step_number = step_number
+        step.duration = step_duration
         step.completed = False
         db.graph.create(step)
         return step
@@ -59,21 +62,21 @@ class Step(db.Model):
 
 class BuildClient(object):
 
-    def __init__(self, client_id, client_email):
-        self.client = Client.create(client_id, client_email)
+    def __init__(self, company_id, company_name):
+        self.client = Client.create(company_id, company_name)
         self.onboard = Onboard.create()
         self.steps = []
         self.tasks = [
-            {'task_name': 'get signed contracts'}, 
-            {'task_name': 'get compliance documents'},
-            {'task_name': 'compliance review'},
-            {'task_name': 'countersign contracts'},
-            {'task_name': 'account activation'},
+            {'task_name': 'get signed contracts', 'duration': 3}, 
+            {'task_name': 'get compliance documents', 'duration': 4},
+            {'task_name': 'compliance review', 'duration': 3},
+            {'task_name': 'countersign contracts', 'duration': 5},
+            {'task_name': 'account activation', 'duration': 3},
         ]
 
     def init_steps(self):
         for step_number, task in enumerate(self.tasks):
-            self.steps.append(Step.create(task['task_name'], step_number))
+            self.steps.append(Step.create(task['task_name'], step_number, task['duration']))
         return self.steps
 
     def init_steps_rels(self):
@@ -107,25 +110,28 @@ class Employee(db.Model):
     email = db.Property()
     first_name = db.Property()
     last_name = db.Property()
+    street_address = db.Property()
+    city = db.Property()
+    state = db.Property()
+    zip_code = db.Property()
 
     works_for = db.RelatedTo('Company')
     worked_on = db.RelatedTo('Project')
+    has_access_to = db.RelatedTo('Application')
 
     @staticmethod
-    def create(employee_id, employee_email, first_name, last_name):
+    def create(employee_id, employee_email):
         employee = Employee()
         employee.person = True
         employee.id = employee_id
         employee.email = employee_email
-        employee.first_name = first_name
-        employee.last_name = last_name
         db.graph.create(employee)
         return employee
 
 class BuildEmployee(object):
     
-    def __init__(self, employee_id, employee_email, first_name, last_name, company_name):
-        self.employee = Employee.create(employee_id, employee_email, first_name, last_name)
+    def __init__(self, employee_id, employee_email, company_name):
+        self.employee = Employee.create(employee_id, employee_email)
         self.company = Company.push(company_name)
 
     def init_rels(self):
@@ -153,7 +159,7 @@ class EmployeeInvolvement(object):
         ).first()
         self.project = Project.create()
         self.client = Client.select(db.graph).where(
-          "_.id='%s'" % client_id
+          "_.company_id='%s'" % client_id
         ).first()
         self.onboard = list(self.client.has_onboard)[0]
 
@@ -173,7 +179,7 @@ class EmployeeAccess(object):
     def update_step_access(self, client_id, step_number):
         return db.graph.run(
             "MATCH (e:Employee)-[:WORKED_ON]->(p:Project)-[:FOR_CLIENT]->(c:Client) " +
-            "WHERE e.id='%s' AND c.id='%s' " % (self.employee_id, client_id) +
+            "WHERE e.id='%s' AND c.company_id='%s' " % (self.employee_id, client_id) +
             "MATCH (c)-[:HAS_ONBOARD]->()-[:HAS_STEP]->(s) " + 
             "WHERE s.step_number=%s " % str(step_number) +
             "MERGE (p)-[:ACCESSED_STEP]->(s) " +
@@ -181,7 +187,171 @@ class EmployeeAccess(object):
         )
 
 
-@app.route('/create/<string:name>')
-def create(name):
-    db.graph.run("CREATE (n:User {name: '%s'})" % name) # noqa
-    return jsonify('user %s created' % name)
+class Application(db.Model):
+
+    __primarykey__ = 'name'
+
+    crm = db.Label()
+    erp = db.Label()
+    compliance = db.Label()
+    cloud = db.Label()
+
+    name = db.Property()
+
+    # accessed_by = db.RelatedFrom('Employee')
+    uses_database = db.RelatedTo('Database')
+
+    @staticmethod
+    def push_crm(app_name):
+        crm_app = Application()
+        crm_app.crm = True
+        crm_app.cloud = True
+        crm_app.name = app_name
+        db.graph.push(crm_app)
+        return crm_app
+
+    @staticmethod
+    def push_erp(app_name):
+        erp_app = Application()
+        erp_app.erp = True
+        erp_app.name = app_name
+        db.graph.push(erp_app)
+        return erp_app
+
+    @staticmethod
+    def push_compliance(app_name):
+        comp_app = Application()
+        comp_app.compliance = True
+        comp_app.name = app_name
+        db.graph.push(comp_app)
+        return comp_app
+        
+
+class Database(db.Model):
+    
+    type = db.Property()
+    
+    in_use_by = db.RelatedFrom('Application')
+
+    @staticmethod
+    def push(database_type):
+        database = Database()
+        database.type = database_type
+        db.graph.push(database)
+        return database
+
+
+class CrmDatabase(object):
+   
+    def __init__(self, app_name, database_type):
+        self.crm_app = Application.push_crm(app_name)
+        self.database = Database.push(database_type)
+
+    def build(self):
+        self.crm_app.uses_database.add(self.database)
+        db.graph.push(self.crm_app)
+        db.graph.push(self.database)
+        return 'structure built'
+
+class ErpDatabase(object):
+   
+    def __init__(self, app_name, database_type):
+        self.erp_app = Application.push_erp(app_name)
+        self.database = Database.push(database_type)
+
+    def build(self):
+        self.erp_app.uses_database.add(self.database)
+        db.graph.push(self.erp_app)
+        db.graph.push(self.database)
+        return 'structure built'
+
+class ComplianceDatabase(object):
+   
+    def __init__(self, app_name, database_type):
+        self.comp_app = Application.push_compliance(app_name)
+        self.database = Database.push(database_type)
+
+    def build(self):
+        self.comp_app.uses_database.add(self.database)
+        db.graph.push(self.comp_app)
+        db.graph.push(self.database)
+        return 'structure built'
+   
+
+class EmployeeAppAccess(object):
+
+    def __init__(self, app_label, employee_id):
+        self.app_label = app_label
+        self.employee_id = employee_id
+
+    def build(self):
+        employee = Employee.select(db.graph).where(
+          "_.id='%s'" % self.employee_id
+        ).first()
+        
+        app = Application.select(db.graph).where(
+          "'%s' IN labels(_)" % self.app_label
+        ).first()
+
+        employee.has_access_to.add(app)
+        db.graph.push(employee)
+        return 'built employee app access'
+
+
+def build_model():
+    
+    client_1 = BuildClient('company_id_1', 'company_name_1')
+    client_1.init_steps()
+    client_1.init_steps_rels()
+    
+    client_2 = BuildClient('company_id_2', 'company_name_2')
+    client_2.init_steps()
+    client_2.init_steps_rels()
+    
+    employee_1 = BuildEmployee('employee_id_1', 'employee_email_1', 'neo4j')
+    employee_2 = BuildEmployee('employee_id_2', 'employee_email_2', 'neo4j')
+    
+    employee_1.init_rels()
+    employee_2.init_rels()
+
+    empl_cust_involve_1 = EmployeeInvolvement('employee_id_1', 'company_id_1')
+    empl_cust_involve_2 = EmployeeInvolvement('employee_id_2', 'company_id_2')
+    
+    empl_cust_involve_1.init_rels()
+    empl_cust_involve_2.init_rels()
+
+    customer_access_1 = EmployeeAccess('employee_id_1')
+    customer_access_2 = EmployeeAccess('employee_id_2')
+
+    customer_access_1.update_step_access('company_id_1', 4)
+    customer_access_2.update_step_access('company_id_2', 2)
+
+    crm = CrmDatabase('salesforce', 'cloud')
+    crm.build()
+
+    erp = ErpDatabase('SAP', 'oracle1')
+    erp.build()
+
+    compliance = ComplianceDatabase('actimize', 'sqlserver1')
+    compliance.build()
+
+    app_access_1 = EmployeeAppAccess('Crm', 'employee_id_1')
+    app_access_1.build()
+    
+    app_access_2 = EmployeeAppAccess('Erp', 'employee_id_2')
+    app_access_2.build()
+    
+    app_access_3 = EmployeeAppAccess('Compliance', 'employee_id_2')
+    app_access_3.build()
+
+    return 'model built'
+
+
+@app.route('/build')
+def build():
+    
+    db.graph.run("MATCH (n) DETACH DELETE n")
+    
+    m = build_model()
+
+    return jsonify({'status': m})
